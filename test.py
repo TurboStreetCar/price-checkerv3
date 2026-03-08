c59f4d21af718241d7855d696132081fcf5ead06
TurboStreetCar <37253084+TurboStreetCar@users.noreply.github.com>
2026-03-08 11:39:44 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
+import os
+
 # filename of the text file
 file_name = "/logs/testlog.log"
-
+user = os.getenv("ROMEOS_USER")
 # text to be added (using \n for a new line)
-text_to_append = "This is a new line added to the file.\n"
+text_to_append = "This user: " + user + " has added a new line added to the file.\n"
 
 try:
     # 'a' mode: Open for appending (creates file if it doesn't exist)
@@ -10,7 +12,7 @@ try:
     with open(file_name, "a", encoding="utf-8") as file:
         file.write(text_to_append)
     
-    print(f"Successfully appended text to {file_name}")
+    print(f"{user}: Successfully appended text to {file_name}")
 
 except Exception as e:
     print(f"An error occurred: {e}")
