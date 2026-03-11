f56f0df08c55e9be3ed4feaa4cfdb411169946ea
TurboStreetCar <37253084+TurboStreetCar@users.noreply.github.com>
2026-03-11 07:49:50 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,16 +7,10 @@ service cron start
 # 2. Export ENV variables for cron
 printenv | grep -v "no_proxy" >> /etc/environment
 
-# 3. Ensure directories exist and copy files
+# 3. Ensure directories exist
 mkdir -p /app/scripts/ /app/www/ /etc/cron.d/ /logs/ 
 
-[ ! -f "/app/streamlit_app.py" ] && cp /tmp/base_app/scripts/streamlit_app.py /app/www/
-[ ! -f "/etc/cron.d/fuel-cron" ] && cp /tmp/base_app/cron/fuel-cron /etc/cron.d/
-[ ! -f "/app/scripts/price-checker.py" ] && cp /tmp/base_app/scripts/price-checker.py /app/scripts/
-
-# 4. Copy latest samples
-cp /tmp/base_app/scripts/test.py /app/scripts/
-cp /tmp/base_app/cron/test-cron /etc/cron.d/
+[ ! -f "/app/www/streamlit_app.py" ] && cp /tmp/base_app/scripts/streamlit_app.py /app/www/
 
 # 5. Set strict permissions (Cron requirement)
 chmod 0644 /etc/cron.d/*
