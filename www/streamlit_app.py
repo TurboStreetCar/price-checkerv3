959283d211cc082ca03101e0a99524e9613331ea
TurboStreetCar <37253084+TurboStreetCar@users.noreply.github.com>
2026-07-05 21:59:01 -0400

    Delete www directory

--- www/streamlit_app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-#st.set_page_config(layout="wide")
-# Define the pages pointing to the files in the pages folder
-oil_prices = st.Page("pages/Oil_Prices.py", title="Oil Prices", icon="🏠", default=True)
-weather = st.Page("pages/Weather_Conditions.py", title="Weather Conditions", icon="🌤️")
-
-# Initialize and run navigation
-pg = st.navigation([oil_prices, weather])
-pg.run()
