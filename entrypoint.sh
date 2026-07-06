86a6bd9210e1f62b9dda703e562582513c15c113
TurboStreetCar <37253084+TurboStreetCar@users.noreply.github.com>
2026-07-05 21:55:05 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,11 @@ service cron start
 printenv | grep -v "no_proxy" >> /etc/environment
 
 # 3. Ensure directories exist
-mkdir -p /app/scripts/ /app/www/ /etc/cron.d/ /logs/ 
+mkdir -p /app/scripts/ /etc/cron.d/ /logs/ 
 
-[ ! -f "/app/www/streamlit_app.py" ] && cp /tmp/base_app/www/streamlit_app.py /app/www/
-
-# 5. Set strict permissions (Cron requirement)
+# 4. Set strict permissions (Cron requirement)
 chmod 0644 /etc/cron.d/*
 touch /var/log/cron.log
 
-# 6. Execute CMD
+# 5. Execute CMD
 exec "$@"
